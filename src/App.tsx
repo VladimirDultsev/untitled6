81d9304d073f90f1d6b28cc317cdafceac9a7f48
VladimirDultsev <vdultsev2009@gmail.com>
2026-03-14 13:08:34 +0300

    Fst Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route, Routes} from "react-router-dom"
+import {AnimalsPage} from './Animals'
+import {AnimalComponent} from './Animal'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Routes>
+              <Route path="/animals">
+                  <Route index element={<AnimalsPage />} />
+                  <Route path={":id"} element={<AnimalComponent />} />
+              </Route>
+          </Routes>
     </div>
   );
 }
